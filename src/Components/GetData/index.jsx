bf7cf32f7c6e145ea3967fa32cd27274e442d35d
MiLunaYunorvi <89271104+MiLunaYunorvi@users.noreply.github.com>
2023-08-15 20:56:55 -0500

    Commiting15

--- src/Components/GetData/index.jsx
+++ src/Components/GetData/index.jsx
@@ -35,7 +35,7 @@ function compararConsultas(ultimasFechas, datosBaseDeDatos) {
 }
 
 async function consultar() {
-  const backendUrl = 'https://miluna.dev/consultas';
+  const backendUrl = 'https://miluna.dev';
   try {
     const response = await axios.get(`${backendUrl}/consultas`);
     const respuesta_DB = response.data;
