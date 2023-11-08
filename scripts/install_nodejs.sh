faf7a5ff0666ac4c5536a421cc112905bbb5aee0
MiLunaYunorvi <89271104+MiLunaYunorvi@users.noreply.github.com>
2023-11-08 16:02:56 -0500

    Actualizado start_app.sh con screen, corrregido v3 :)

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -7,6 +7,6 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 apt-get update
-apt-get install screen -y
+#apt-get install screen -y
 apt-get install nodejs -y
 
