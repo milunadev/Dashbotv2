eeef07d6bc793e1ba14d01216b5cdb27509719f7
MiLunaYunorvi <89271104+MiLunaYunorvi@users.noreply.github.com>
2023-11-09 11:48:37 -0500

    Dividiendo la instalaciòn de paquetes para disminuir la carga v3

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,37 +1,5 @@
+#!/bin/bash
 cd /var/app
-rm -rf /var/app/node_modules/*
-# Instalación de dependencias principales
-npm install \
-  @emotion/react@^11.11.1 \
-  @emotion/styled@^11.11.0 \
-  @floating-ui/react@^0.25.4 \
-  @fortawesome/fontawesome-svg-core@^6.4.2 \
-  @fortawesome/free-brands-svg-icons@^6.4.2 \
-  @fortawesome/free-solid-svg-icons@^6.4.2 \
-  @fortawesome/react-fontawesome@^0.2.0 \
-  @mui/material@^5.14.8 \
-  apexcharts@^3.41.1 \
-  atropos@^2.0.2 \
-  aws-sdk@^2.1429.0 \
-  axios@^1.4.0 \
-  chart.js@^4.3.3 \
-  concurrently@^8.2.0 \
-  cors@^2.8.5 \
-  express@^4.18.2 \
-  framer-motion@^10.16.4 \
-  mysql2@^3.6.0 \
-  npm-run-all@^4.1.5 \
-  react@^18.2.0 \
-  react-apexcharts@^1.4.1 \
-  react-dom@^18.2.0 \
-  react-router-dom@^6.15.0 \
-  tailwindcss@^3.3.3 \
-  web-vitals@^2.1.4
+echo "Installing all dependencies using npm install..."
+npm ci
 
-# Instalación de dependencias de desarrollo y pruebas
-npm install \
-  @testing-library/jest-dom@^5.17.0 \
-  @testing-library/react@^13.4.0 \
-  @testing-library/user-event@^13.5.0 \
-  react-scripts@5.0.1 \
-  --save-dev
