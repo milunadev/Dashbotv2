02b495dfa2debeae3d5f389fe7c59f14d5301875
MiLunaYunorvi <89271104+MiLunaYunorvi@users.noreply.github.com>
2023-11-10 10:16:41 -0500

    En branch AboutMe: quitando appspec y scripts, agregando buildspec.yml

--- scripts/install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /var/app # Cambia al directorio raíz de tu proyecto
-npm install
--- scripts/start_app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-screen -dmS myapp bash -c 'cd /var/app && PORT=8080 npm run start:both'
-
