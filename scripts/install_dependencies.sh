5a200310c8cea47801f5c7a93ff2a578d5013d07
MiLunaYunorvi <89271104+MiLunaYunorvi@users.noreply.github.com>
2023-11-10 14:46:29 -0500

    exportando solo buil, usando nginx en EC2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-cd /var/app
-echo "Installing all dependencies using npm install..."
-npm install
-npm install pm2 -g
+apt-get install nginx -y
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-cd /var/app
-pm2 delete dashbot
-pm2 start npm --name "dashbot" -- run start:both
-pm2 startup
-pm2 save
-pm2 restart dashbot
+# cd /var/app
+# pm2 delete dashbot
+# pm2 start npm --name "dashbot" -- run start:both
+# pm2 startup
+# pm2 save
+# pm2 restart dashbot
+
+
+#USANDO NGINGX
+cp -r /var/app/build /var/www/html
+cat << 'EOF' | sudo tee /etc/nginx/nginx.conf
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+        worker_connections 768;
+        # multi_accept on;
+}
+
+http {
+        server{
+          listen 8080;
+          root /var/www/html/build;
+        }
+}
+EOF
+service nginx restart
