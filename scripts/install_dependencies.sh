b8933d40be06f81f2acb2ddf6d1b226acc7aa82a
MiLunaYunorvi <89271104+MiLunaYunorvi@users.noreply.github.com>
2023-11-10 13:13:24 -0500

    En AboutMe, agregando server al buildspec

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /var/app
 echo "Installing all dependencies using npm install..."
-screen -ls | awk '/[0-9]+\./ {print $1}' | xargs -I {} sh -c 'screen -X -S {} quit'
-npm install pm2 -g
 npm install
+npm install pm2 -g
